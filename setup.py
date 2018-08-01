e6f5758e449f9f5dc88dc14f1ffd0ec1fde241d0
Jim Schwoebel <30424731+jim-schwoebel@users.noreply.github.com>
2018-08-01 18:07:05 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,13 @@ For installing all dependencies for nala.
 '''
 import os
 
+# need to fix pocketsphinx for custom language model 
+# https://github.com/watsonbox/homebrew-cmu-sphinx
+os.system('brew uninstall pocketsphinx')
+os.system('brew tap watsonbox/cmu-sphinx')
+os.system('brew install --HEAD watsonbox/cmu-sphinx/cmu-sphinxbase')
+os.system('brew install --HEAD watsonbox/cmu-sphinx/cmu-pocketsphinx')
+
 def pip_install(modules):
     for i in range(len(modules)):
         os.system('pip3 install %s'%(modules[i]))
@@ -16,28 +23,15 @@ def brew_install(modules):
 brew_modules=['portaudio',
               'shpotify']
 
-pip_modules=['',
-             '',
-             '',
-             '',
-             '',
-             '',
-             '',
-             '',
-             '',
-             '']
+pip_modules=['ftplib', 'smtplib', 'getpass', 'pyaudio','pygame'
+             'wave','shutil','importlib','geocoder','librosa',
+             'urrllib','random','webbrowser','pyperclip','pydub',
+             'array','struct','soundfile','pandas','numpy',
+             'bs4','cryptography','pyscreenshot','pyautogui',
+             'cv2','readchar','psutil','SpeechRecognition',
+             'pyttsx3','soundfile','skvideo','moviepy','PIL']
 
 brew_install(brew_modules)
 pip_install(pip_modules)
-
-# need to fix pocketsphinx for custom language model 
-# https://github.com/watsonbox/homebrew-cmu-sphinx
-os.system('brew uninstall pocketsphinx')
-os.system('brew tap watsonbox/cmu-sphinx')
-os.system('brew install --HEAD watsonbox/cmu-sphinx/cmu-sphinxbase')
-os.system('brew install --HEAD watsonbox/cmu-sphinx/cmu-pocketsphinx')
-
-# test pocketsphinx with this command (can exit out of screen whenever).
-os.system('pocketsphinx_continuous -inmic yes')
 
 # done! now you're ready
