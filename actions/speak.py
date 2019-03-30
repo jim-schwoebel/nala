e7c5c17449189c5780fe17eadccced27f7abec1f
Jim Schwoebel <30424731+jim-schwoebel@users.noreply.github.com>
2019-03-30 15:43:32 -0400

    Update speak.py

--- actions/speak.py
+++ actions/speak.py
@@ -64,9 +64,14 @@ import sys
 import pyttsx3 as pyttsx
 
 def say(text):
-    engine = pyttsx.init()
-    engine.setProperty('voice','com.apple.speech.synthesis.voice.fiona')
-    engine.say(text)
-    engine.runAndWait()
+    try:
+         engine = pyttsx.init()
+         engine.setProperty('voice','com.apple.speech.synthesis.voice.fiona')
+         engine.say(text)
+         engine.runAndWait()
+         say(str(sys.argv[1]))
+     except:
+         #backup to using python2 if python3 does not work
+         os.system('python speak.py sys.argv[0] sys.argv[1]')
 
-say(str(sys.argv[1]))
+
